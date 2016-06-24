c621cf30049cae115dfa02657d667ebdb657e14b
1111hui <26634873@qq.com>
2016-06-24 17:50:31 +0800

    first version

---
+++ amd/cssobj-plugin-post-stylize.js
@@ -0,0 +1,34 @@
+define('cssobj_plugin_post_stylize', function () { 'use strict';
+
+  function stylize (element, sheet) {
+    element.type = 'text/css'
+    if (element.styleSheet) {
+      element.styleSheet.cssText = sheet
+    } else {
+      // empty all style when re-apply new style
+      while(element.firstChild) element.removeChild(element.firstChild)
+      element.appendChild(document.createTextNode(sheet))
+    }
+    return element
+  }
+
+  var styleDom
+  function addStyleToHead (sheet, option) {
+    var id = 'head_style_cssobj'
+    if (!styleDom) {
+      var el = document.createElement('style')
+      document.head.appendChild(el)
+      styleDom = el
+    }
+    styleDom.setAttribute('id', id.replace(/\"/g, '&quot;').replace(/\'/g, '&apos;'))
+    styleDom.setAttribute('type', 'text/css')
+    if(option && typeof option=='object' && option.attrs)
+      for(var i in option.attrs){
+        styleDom.setAttribute(i, option.attrs[i])
+      }
+    return stylize(styleDom, sheet.css)
+  }
+
+  return addStyleToHead;
+
+});
---
+++ src/cssobj-plugin-post-stylize.js
@@ -0,0 +1,31 @@
+
+
+function stylize (element, sheet) {
+  element.type = 'text/css'
+  if (element.styleSheet) {
+    element.styleSheet.cssText = sheet
+  } else {
+    // empty all style when re-apply new style
+    while(element.firstChild) element.removeChild(element.firstChild)
+    element.appendChild(document.createTextNode(sheet))
+  }
+  return element
+}
+
+var styleDom
+export default function addStyleToHead (sheet, option) {
+  var id = 'head_style_cssobj'
+  if (!styleDom) {
+    var el = document.createElement('style')
+    document.head.appendChild(el)
+    styleDom = el
+  }
+  styleDom.setAttribute('id', id.replace(/\"/g, '&quot;').replace(/\'/g, '&apos;'))
+  styleDom.setAttribute('type', 'text/css')
+  if(option && typeof option=='object' && option.attrs)
+    for(var i in option.attrs){
+      styleDom.setAttribute(i, option.attrs[i])
+    }
+  return stylize(styleDom, sheet.css)
+}
+
