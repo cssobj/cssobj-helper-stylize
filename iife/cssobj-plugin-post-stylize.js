a240544ac01b0e5e816ddffe6c339e5e04d61db9
1111hui <26634873@qq.com>
2016-06-24 19:56:12 +0800

    CHANGE: remove uglify for iife

--- iife/cssobj-plugin-post-stylize.js
+++ iife/cssobj-plugin-post-stylize.js
@@ -1,1 +1,35 @@
-var cssobj_plugin_post_stylize=function(){"use strict";function t(t,e){if(t.type="text/css",t.styleSheet)t.styleSheet.cssText=e;else{for(;t.firstChild;)t.removeChild(t.firstChild);t.appendChild(document.createTextNode(e))}return t}function e(e,r){var i="head_style_cssobj";if(!s){var c=document.createElement("style");document.head.appendChild(c),s=c}if(s.setAttribute("id",i.replace(/\"/g,"&quot;").replace(/\'/g,"&apos;")),s.setAttribute("type","text/css"),r&&"object"==typeof r&&r.attrs)for(var a in r.attrs)s.setAttribute(a,r.attrs[a]);return t(s,e.css)}var s;return e}();
+var cssobj_plugin_post_stylize = (function () {
+  'use strict';
+
+  function stylize (element, sheet) {
+    element.type = 'text/css'
+    if (element.styleSheet) {
+      element.styleSheet.cssText = sheet
+    } else {
+      // empty all style when re-apply new style
+      while(element.firstChild) element.removeChild(element.firstChild)
+      element.appendChild(document.createTextNode(sheet))
+    }
+    return element
+  }
+
+  var styleDom
+  function addStyleToHead (sheet, option) {
+    var id = 'head_style_cssobj'
+    if (!styleDom) {
+      var el = document.createElement('style')
+      document.head.appendChild(el)
+      styleDom = el
+    }
+    styleDom.setAttribute('id', id.replace(/\"/g, '&quot;').replace(/\'/g, '&apos;'))
+    styleDom.setAttribute('type', 'text/css')
+    if(option && typeof option=='object' && option.attrs)
+      for(var i in option.attrs){
+        styleDom.setAttribute(i, option.attrs[i])
+      }
+    return stylize(styleDom, sheet.css)
+  }
+
+  return addStyleToHead;
+
+}());
