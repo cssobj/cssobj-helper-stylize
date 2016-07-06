be3fec83de7df68971bc6ca23154278a1b8de459
1111hui <26634873@qq.com>
2016-07-06 18:29:23 +0800

    ADD: build for update

--- cjs/cssobj-plugin-post-stylize.js
+++ cjs/cssobj-plugin-post-stylize.js
@@ -6,10 +6,10 @@
  * @author James Yang [jamesyang999@gmail.com]
  * @license MIT
  * @usage
-cssobj(obj, {
-  post:[cssobj_plugin_post_stylize({name:'gaga', attrs: {media: 'screen'}})]
-})
- */
+ cssobj(obj, {
+ post:[cssobj_plugin_post_stylize({name:'gaga', attrs: {media: 'screen'}})]
+ })
+*/
 
 function escapeHTML (str) {
   return str.replace(/&/g, '&amp;')
@@ -20,7 +20,7 @@ function escapeHTML (str) {
 }
 
 function stylize (element, sheet) {
-  if(element.cachedCSS===sheet) return
+  if (element.cachedCSS === sheet) return
   element.cachedCSS = sheet
   if (element.styleSheet) {
     element.styleSheet.cssText = sheet
@@ -32,24 +32,31 @@ function stylize (element, sheet) {
   return element
 }
 
+function createDOM (id, option) {
+  var el = document.createElement('style')
+  document.head.appendChild(el)
+  el.setAttribute('id', id)
+  if (option && typeof option == 'object' && option.attrs)
+    for (var i in option.attrs) {
+      el.setAttribute(i, option.attrs[i])
+    }
+  return el
+}
+
 function addStyleToHead (option) {
   option = option || {}
-  if (!option.name) option.name = +new Date() + '_'
-  return function (sheet) {
-    var id = 'style_cssobj_' + escapeHTML(option.name)
-    var styleDom = document.getElementById(id)
-    if (!styleDom) {
-      var el = document.createElement('style')
-      document.head.appendChild(el)
-      styleDom = el
+  if (!option.name) option.name = +new Date() + ''
+  var id = 'style_cssobj_' + escapeHTML(option.name)
+  var styleDom = document.getElementById(id) || createDOM(id, option)
+  return function (result) {
+    if (Object.keys(result.vars).length) {
+      var updateID = id + '_update'
+      var updateDom = document.getElementById(updateID) || createDOM(updateID, option)
+      result.options.on('update', function (css) {
+        stylize(updateDom, css)
+      })
     }
-    styleDom.setAttribute('id', id)
-    styleDom.setAttribute('type', 'text/css')
-    if (option && typeof option == 'object' && option.attrs)
-      for (var i in option.attrs) {
-        styleDom.setAttribute(i, option.attrs[i])
-    }
-    return stylize(styleDom, sheet.css)
+    return stylize(styleDom, result.css)
   }
 }
 
